bee4744ab4b5c03bb7c8b6f742fbe0d3b1153096
Ahmetdlpr <mybqadresadrs@outlook.com>
2024-10-17 16:48:09 +0300

    Değişiklikler eklendi

--- Baist kayıt arayüzü/Form1.Designer.cs
+++ Baist kayıt arayüzü/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1085, 450);
+            ClientSize = new Size(1378, 492);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
